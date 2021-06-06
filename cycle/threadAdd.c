0680782f75d4052a8e3d9893949fafe03746fef8
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2021-06-06 15:19:34 +0530

    Create threadAdd.c

---
+++ cycle/threadAdd.c
@@ -0,0 +1,37 @@
+/*
+Program to create a thread called add, 
+which access two integers from the user
+and print the sum. 
+*/
+
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+void* add(void* arg)
+{
+ 
+  int i,num1,num2;
+
+  printf("\nEnter the numbers :-->\n");
+  scanf("%d%d",&num1,&num2);
+  printf("%d\n",num1+num2);
+ 
+}
+void print(int *result)
+{
+  
+  printf("Sum =  %d",*result);
+}
+
+int main()
+{
+  
+  
+  pthread_t  newThread;
+  pthread_create(&newThread, NULL, add,  
+  NULL);
+  pthread_join(newThread,NULL);
+
+
+}
